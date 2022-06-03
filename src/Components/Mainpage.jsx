ca060adbc0307f69cbc55b5da41522df662eb7c4
Aguda <agudaoluwanifemi@gmail.com>
2022-06-03 15:09:18 +0100

    second commit

--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Mainpage.css'
-import Polygon from '../assets/Polygon 1.svg'
+// import Polygon from '../assets/Polygon 1.svg'
 import logo from '../assets/logo.svg'
 import image from '../assets/image.svg'
 import play from '../assets/play.svg'
@@ -23,10 +23,36 @@ const mainpage = () => {
             </div>
 
             <div className="right">
-              <button className='button'>LOG IN</button>
-              <button className='button sign-in'>GET STARTED</button>
+              <p className='button'>LOG IN</p>
+              <p className='sign-in'>GET STARTED</p>
             </div>
         </div>
+
+        <div className="bod">
+          <div className="body-txt">
+            <h2 className='txt-header'>Automate Routine Work</h2>
+            <p className='txt-info'>Automate everyday tasks like sharing updates and alternate any process from onboarding to gathering feedback.</p>
+           <div className="clickers">
+             <p className='sign-in'>GET STARTED</p>
+            <div className="play">
+              <img src={play} alt="" />
+              <p className='how'>How it works</p>
+            </div>
+           </div>
+          </div>
+
+          <img className='image' src={image} alt="" />
+        </div>
+        <footer>
+          <p>www.pixel.com</p>
+
+          <div className="socials">
+            <p>Follow us</p>
+            <img src={twitter} alt="" />
+            <img src={instagram} alt="" />
+            <img src={facebook} alt="" />
+          </div>
+        </footer>
       </div>
     </div>
         
